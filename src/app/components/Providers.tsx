82507a96a1d1defd3566db2bfd4baef8fef40c71
GargDivanshu <divanshu.garg113@gmail.com>
2024-05-23 11:39:47 +0530

    err resolution

--- src/app/components/Providers.tsx
+++ src/app/components/Providers.tsx
@@ -1,4 +1,5 @@
 'use client'
+
 import { PropsWithChildren, useState } from "react"
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 import { trpc } from "../_trpc/client"
@@ -11,7 +12,7 @@ const Providers = ({children}: PropsWithChildren) => {
     const [trpcClient] = useState(() => trpc.createClient({
         links: [
             httpBatchLink({
-                url: 'https://9000-idx-chatpdf-1716143585263.cluster-3g4scxt2njdd6uovkqyfcabgo6.cloudworkstations.dev/api/trpc',
+                url: `${process.env.KINDE_SITE_URL}/api/trpc`,
                 // https://9000-idx-chatpdf-1716143585263.cluster-3g4scxt2njdd6uovkqyfcabgo6.cloudworkstations.dev/auth-callback?origin=dashboard
             }),
         ],
